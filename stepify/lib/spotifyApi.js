22aa3117bb367aee65ad2ed8f101c458758e753e
carballo <carballet@gmail.com>
2020-02-19 20:09:07 +0100

    API de Spotify terminada

--- stepify/lib/spotifyApi.js
+++ stepify/lib/spotifyApi.js
@@ -1,37 +1,40 @@
 require("dotenv").config();
 const SpotifyWebApi = require('spotify-web-api-node');
 
-// AQUÍ FALTA UN LET PARA GUARDAR EL ID DEL ARTISTA CON EL QUE QUERAMOS TRABAJAR
-// NO ESTOY SEGURO DE QUE LOS EXPORTS ESTÉN BIEN
-// HAY QUE DESARROLLAR EL CÓMO LLAMAR A ESTAS FUNCIONES DESDE EL DOM
-
 const spotiClientId = process.env.SPOTICLIENTID;
 const spotiSecret = process.env.SPOTICLIENTSECRET;
 
-//spotifyApi.setAccessToken('<your_access_token>');
-
-const spotifyApi = new SpotifyWebApi({
+let credentials = {
   clientId: spotiClientId,
   clientSecret: spotiSecret,
-  redirectUri: '/' // Esta es la URL de callback a la que responderá
-});
+}
 
-// Devuelve artistas relacionados
+const spotifyApi = new SpotifyWebApi(credentials);
 
-spotifyApi.getArtistRelatedArtists('0qeei9KQnptjwb8MgkqEoy')
-  .then(function (data) {
-    console.log(data.body);
-  }, function (err) {
-    done(err);
-  });
+// // Devuelve artistas relacionados
+
+const spotiGetArtistRelatedArtists = () => {
+  spotifyApi.getArtistRelatedArtists('0uq5PttqEjj3IH1bzwcrXF')
+    .then(function (data) {
+      console.log(data.body);
+    }, function (err) {
+      console.error(err);
+    });
+}
 
 // Devuelve datos de un artista
 
-spotifyApi.getArtist('2hazSY4Ef3aB9ATXW7F5w3')
-  .then(function (data) {
-    console.log('Artist information', data.body);
-  }, function (err) {
-    console.error(err);
-  });
+const spotiGetArtist = () => {
+  spotifyApi.getArtist('2hazSY4Ef3aB9ATXW7F5w3')
+    .then(function (data) {
+      console.log('Artist information', data.body);
+    }, function (err) {
+      console.error(err);
+    });
+}
 
-module.exports = spotifyApi;
+module.exports = {
+  spotifyApi,
+  spotiGetArtistRelatedArtists,
+  spotiGetArtist
+};
