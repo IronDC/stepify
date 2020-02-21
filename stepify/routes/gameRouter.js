8a2d29e94f52f4ff330fc0e4a88660aaaefdb66d
David Garcia <dgarciabravo13@gmail.com>
2020-02-21 20:07:59 +0100

    working in initialArtist

--- stepify/routes/gameRouter.js
+++ stepify/routes/gameRouter.js
@@ -3,16 +3,30 @@ const express = require("express");
 const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
 
-router.get("/start", ensureLogin.ensureLoggedIn(), async (req,res,next) => {
-  try{
+router.get("/start", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
+  try {
     const artist = await Artist.find();
+    const selectRandom = array =>
+      array[Math.floor(Math.random() * array.length)];
+
+    Array.prototype.removeItem = function(a) {
+      for (let i = 0; i > array.length; i++) {
+        if (this[i] == a) {
+          this.splice(i, 1);
+          return;
+        }
+      }
+    };
+    const cloneArtist = [...artist];
+    const initialArtist = selectRandom(cloneArtist);
+    console.log(initialArtist);
     console.log(`the colecction artist ${artist}`);
-    return res.render("start", {artist, user: req.user});
-  }catch(err){
-    res.send(`Error listing artist": ${err}`)
+    console.log(`the clone colecction artist ${cloneArtist}`);
+    return res.render("start", { artist, user: req.user });
+  } catch (err) {
+    res.send(`Error listing artist": ${err}`);
     next();
   }
-
 });
 
 module.exports = router;
