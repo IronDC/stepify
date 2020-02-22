629235335315027b2f363d3ef5bc55002542cde2
carballo <carballet@gmail.com>
2020-02-22 14:33:48 +0100

    Se crea la página, crea la sesion en GameSession y guarda el initArtist y finalArtist, falta pedir los relacionados y mostrarlos

--- stepify/routes/gameRouter.js
+++ stepify/routes/gameRouter.js
@@ -1,44 +1,118 @@
 const Artist = require("../models/artist");
+const Gamesession = require("../models/gameSession");
 const express = require("express");
 const router = express.Router();
 const ensureLogin = require("connect-ensure-login");
 
-
-
 router.get("/start", ensureLogin.ensureLoggedIn(), async (req, res, next) => {
   try {
     const artist = await Artist.find();
-    const cloneArtist = [... artist];
+    const cloneArtist = [...artist];
     const selectRandom = array =>
-       array[Math.floor(Math.random() * array.length)];
+      array[Math.floor(Math.random() * array.length)];
 
-    // Array.prototype.removeItem = function(a) {
-    //   for (let i = 0; i > array.length; i++) {
-    //     if (this[i] == a) {
-    //       this.splice(i, 1);
-    //       return;
-    //     }
-    //   }
-    // };
-    
     let initialArtist = selectRandom(cloneArtist);
-    // cloneArtist.removeItem(initialArtist);
+
     let index = cloneArtist.indexOf(initialArtist);
-    console.log(`esto es index ${index}`);
-    console.log("Llega antes del IF")
-    if (index > -1){
+
+    if (index > -1) {
       cloneArtist.splice(index, 1);
-      console.log(cloneArtist);
     }
     const finalArtist = selectRandom(cloneArtist);
-    // console.log(`Este es el artista inicial ${initialArtist}`);
-    // console.log(`the colecction artist ${artist}`);
-    // console.log(`the clone colecction artist ${cloneArtist}`);
+
     return res.render("start", { initialArtist, finalArtist, user: req.user });
   } catch (err) {
     res.send(`Error listing artist": ${err}`);
     next();
   }
 });
+
+router.post(
+  "/rel-page/:initArtist&:finalArtist",
+  ensureLogin.ensureLoggedIn(),
+  async (req, res, next) => {
+    try {
+      const { initArtist, finalArtist } = req.params;
+
+      const initialArtist = await Artist.findOne({ idSpotify: initArtist });
+      let initSpoti;
+      let initName;
+      let initImage;
+
+      for (const initArt in initialArtist) {
+        if (initialArtist.idSpotify) {
+          initSpoti = initialArtist.idSpotify;
+        }
+        if (initialArtist.name) {
+          initName = initialArtist.name;
+        }
+        if (initialArtist.image) {
+          initImage = initialArtist.image;
+        }
+      }
+      console.log(
+        `INICIAL GUARDADO ---> ${initName}, ${initSpoti}, ${initImage}`
+      );
+      const endArtist = await Artist.findOne({ idSpotify: finalArtist });
+
+      let endSpoti;
+      let endName;
+      let endImage;
+
+      for (const endArt in endArtist) {
+        if (endArtist.idSpotify) {
+          endSpoti = endArtist.idSpotify;
+        }
+        if (initialArtist.name) {
+          endName = endArtist.name;
+        }
+        if (endArtist.image) {
+          endImage = endArtist.image;
+        }
+      }
+      console.log(`FINAL GUARDADO ---> ${endName}, ${endSpoti}, ${initImage}`);
+
+      await Gamesession.create([
+        {
+          initArtist: {
+            idSpotify: initSpoti,
+            name: initName,
+            image: initImage
+          },
+          endArtist: { idSpotify: endSpoti, name: endName, image: endImage },
+          artistArray: []
+        }
+      ]);
+
+      console.log(`CREATE OK --- redireccionando...`);
+      return res.redirect(`/rel-page/${initArtist}&${finalArtist}`);
+    } catch (err) {
+      res.send(`Error retrieving Rel Page": ${err}`);
+      next();
+    }
+  }
+);
+
+router.get(
+  "/rel-page/:initArtist&:finalArtist",
+  ensureLogin.ensureLoggedIn(),
+  async (req, res, next) => {
+    try {
+      const { initArtist, finalArtist } = req.params;
+      const initialArtist = await Artist.findOne({ idSpotify: initArtist });
+      // pedir los relacionados de spotify de initialArtist
+      const finArtist = await Artist.findOne({ idSpotify: finalArtist });
+      return res.render("rel-page", {
+        initialArtist,
+        finArtist,
+        // enviar los relacionados de spotify
+        user: req.user
+      });
+    } catch (err) {
+      res.send(`Error retrieving Rel Page": ${err}`);
+      next();
+    }
+  }
+);
 
 module.exports = router;
